bcf805326fd7fba390259fe2c9a2bddc9aa288c4
Tenager-Pro <65409198+Tenager-Pro@users.noreply.github.com>
2022-09-11 16:19:14 +0300

    Переспективные языки программирования бд 2 лаба

---
+++ Переспективные языки прораммирования/ЛБ2/new.php
@@ -0,0 +1,12 @@
+
+<?php
+$nameTv="";
+$time="";
+$nameFilm="";
+
+$nameTv = $_POST['nameTv'];
+$time = $_POST['time'];
+$nameFilm = $_POST['nameFilm'];
+echo "name: $nameTv";
+?>
+
---
+++ Переспективные языки прораммирования/ЛБ2/program.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<meta charset="utf-8">
+        <title>Россия 1</title>
+        <link rel="icon" href="icon.ico" type="image/x-icon" >
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+ 
+ 
+    <body>
+        
+        <div class="header">
+            <h1><a href="index.html">Россия 1</a></h1>
+            
+        
+        </div>
+        
+        <div class="menu">
+            
+            <h1>Menu</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Телепрограмма</a></li> 
+                    <li><a href="company.html">О компании</a></li>
+                    <li><a href="contact.html">Контакты</a></li>
+                     
+                </ul>
+            </nav>
+        </div>
+        <div class="list"> 
+            <table border="1" width="100%" cellpadding="5">
+                <tr>
+                    <th></th>
+                    <th>8(4842)5342312</th>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <th>russia@smotrim.ru</th>
+                </tr>
+                <tr>
+                    <th>Адрес:</th>
+                    <th>Москва, 5-я ул. Ямского Поля, 19-21</th>
+                </tr>
+                <tr>
+                    <td colspan="2">Адрес:</td>
+                    
+                </tr>
+            </table>
+            <div class="newProgram">
+               
+                <form action="new.php" method="post">
+                <p>Телеканал: <input name="nameTv" type="text"></p>
+                <p>Время: <input name="time" type="text"></p>
+                <p>Название: <input name="nameFilm" type="text"></p>
+                <p><input type='submit' value='Добавить'></p>
+                </form>
+
+               
+                
+
+            </div>
+        </div>
+        
+        
+
+    </body>
+   
+
+
+
+</html>
